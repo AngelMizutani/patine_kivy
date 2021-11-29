bb80e6f0ebd9dd3076d118f144a149c822e32e97
angelMizutani <angelisayuri@yahoo.com.br>
2021-11-29 18:21:22 -0300

    criadas telas de login e cadastro em kivymd

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+
+
+from kivymd.app import MDApp
+from kaki.app import App
+from kivy.factory import Factory
+from kivy.core.window import Window
+
+# main app class for kaki app with kivymd modules
+class LiveApp(MDApp, App):
+    Window.size = (400, 600)
+
+    DEBUG = 1 # set this to 0 make live app not working
+
+    # *.kv files to watch
+    KV_FILES = {
+        os.path.join(os.getcwd(), "view/screenmanager.kv"),
+        os.path.join(os.getcwd(), "view/tela_exercicio/telaExercicio.kv"),
+        os.path.join(os.getcwd(), "view/tela_cadastro/telaCadastro.kv"),
+    }
+
+    # class to watch from *.py files
+    CLASSES = {
+        "MainScreenManager": "view.screenmanager",
+        "TelaExercicio": "view.tela_exercicio.telaExercicio",
+        "TelaCadastro": "view.tela_cadastro.telaCadastro"
+    }
+
+    # auto reload path
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+
+
+    def build_app(self, **kwargs):
+        self.theme_cls.primary_palette = "Purple"
+        return Factory.MainScreenManager()
+
+
+# finally, run the app
+if __name__ == "__main__":
+    LiveApp().run()
